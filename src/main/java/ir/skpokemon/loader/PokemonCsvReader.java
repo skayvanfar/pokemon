5a88d1fc49fea05fe17e205d89d676c6ab887391
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-11-16 11:34:13 +0330

    readWholeCsv is added.

--- src/main/java/ir/skpokemon/loader/PokemonCsvReader.java
+++ src/main/java/ir/skpokemon/loader/PokemonCsvReader.java
@@ -1,7 +1,6 @@
 package ir.skpokemon.loader;
 
 import ir.skpokemon.loader.model.Pokemon;
-import lombok.Cleanup;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,32 +15,33 @@ import java.util.List;
  * Created by sad.kayvanfar on 11/16/2021.
  */
 @Slf4j
-public class CsvReader {
+public class PokemonCsvReader {
 
     private final static String SEPARATOR = ",";
 
     private File file;
 
-    public CsvReader(@NonNull File file) {
+    public PokemonCsvReader(@NonNull File file) {
         this.file = file;
     }
 
     public List<Pokemon> read(int skipLines) throws IOException {
         List<Pokemon> pokemons = new ArrayList<>();
-        @Cleanup BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-        String str;
-        int counter = 0;
-        int index = 1;
-        while ((str = bufferedReader.readLine()) != null) {
-            if (counter++ < skipLines)
-                continue;
-            pokemons.add(parse(str,index++));
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String str;
+            int counter = 0;
+            int index = 1;
+            while ((str = br.readLine()) != null) {
+                if (counter++ < skipLines)
+                    continue;
+                pokemons.add(parse(str, index++));
+            }
         }
         return pokemons;
     }
 
     private Pokemon parse(String str, long id) {
         String[] splitted = str.split(SEPARATOR);
-        return new Pokemon(id, splitted[0], Long.valueOf(splitted[1].trim()), Long.valueOf(splitted[2].trim()));
+        return new Pokemon(id, splitted[0], Long.valueOf(splitted[1]), Long.valueOf(splitted[2]));
     }
 }
---
+++ src/test/java/ir/skpokemon/loader/PokemonCsvReaderTest.java
@@ -0,0 +1,52 @@
+package ir.skpokemon.loader;
+
+import ir.skpokemon.loader.model.Pokemon;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Created by sad.kayvanfar on 11/16/2021.
+ */
+public class PokemonCsvReaderTest {
+
+    private PokemonCsvReader pokemonCsvReader;
+
+    @Before
+    public void setUp() throws Exception {
+        URL fileURL = PokemonCsvReaderTest.class.getClassLoader().getResource("test.csv");
+        Path path = Paths.get(Objects.requireNonNull(fileURL).toURI());
+        pokemonCsvReader = new PokemonCsvReader(path.toFile());
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testReadSizeOfCvs() throws IOException {
+        int expectedCount = 4;
+        int result = pokemonCsvReader.read(1).size();
+        Assert.assertEquals(expectedCount, result);
+    }
+
+    @Test
+    public void readWholeCsv() throws IOException {
+        List<Pokemon> expectedList = Arrays.asList(new Pokemon(1L, "ali", 37L, 88L),
+                new Pokemon(2L, "saeed", 33L, 67L),
+                new Pokemon(3L, "reza", 26L, 70L),
+                new Pokemon(4L, "hossein", 25L, 60L));
+        List<Pokemon> result = pokemonCsvReader.read(1);
+        Assert.assertEquals(expectedList, result);
+    }
+
+}
